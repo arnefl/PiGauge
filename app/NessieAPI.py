0469a61b07b6753cba7d263c95b7010a8cf4867c
Arne F. Lyshol <arnelyshol@gmail.com>
2019-12-08 12:13:30 +0100

    Added serial reporting

--- app/NessieAPI.py
+++ app/NessieAPI.py
@@ -16,31 +16,38 @@ def post_regobs(data_json):
 
     request_url = 'https://api.nokken.net/v0/regobs'
 
-    def _do_post(n_attempt):
+
+    # Lan post
+    def _post_lan(n_attempt):
         request = rq.post(request_url, data=data)
 
         # Pass successful request or raise error
         if request.status_code == 200:
             return 1
         elif request.status_code in [429]:
-            # This status_code will only be returned from Nessie if the user makes to many requests
-            # with a valid apikey.
-            retry_after = 120
-            print('Got error status {}. Will retry once after Nessie\'s requested time out.'.
-                  format(request.status_code))
-
-            # Cap max retires
+            # This status_code will only be returned from Nessie if the user 
+            # makes to many requests with a valid apikey.
             if n_attempt <= 2:
-                time.sleep(retry_after)
+                time.sleep(120)
                 return _do_post(n_attempt + 1)
             else:
-                raise ValueError('An error occurred. Max attempts reached. Nessie API error code {}.'.
-                                 format(request.status_code))
+                return request.status_code
                                  
         elif request.status_code is not None:
-            raise ValueError('Error {}.'.format(request.status_code))
+            return request.status_code
         else:
-            raise ValueError('There was an unknown problem getting observations from Nessie.')
+            return 0
 
-    return _do_post(1)
-    
+
+    def _post_serialat(n_attempt):
+        # TODO: Implement
+        return 0
+
+
+    # Do post
+    if config['reporting']['connection'] == 'lan':
+        return _post_lan(1)
+    elif config['reporting']['connection'] == 'serial':
+        return _post_serialat(1)
+    else:
+        return 0
